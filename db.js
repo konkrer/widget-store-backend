ab94837a6a6620691907542ee0fc6b2476a5a6a2
konkrer <rivortex@gmail.com>
2021-02-14 17:37:22 -0800

    pg client added ssl for heroku

--- db.js
+++ db.js
@@ -3,10 +3,24 @@
 const { Client } = require('pg');
 const { DB_URI } = require('./config');
 
+/** When deployed to Heroku DB_URI will start with "postgres://" -
+ * if so set ssl to configuration object as below.
+ *
+ * Otherwise set ssl null for development.  */
+const ssl = DB_URI.startsWith('postgres://')
+  ? { rejectUnauthorized: false }
+  : null;
+
+/** Create Client */
 const client = new Client({
   connectionString: DB_URI,
+  ssl,
 });
 
-client.connect();
+/** Connect Client to DB or throw error */
+client.connect().catch(err => {
+  console.error(err);
+  throw err;
+});
 
 module.exports = client;
